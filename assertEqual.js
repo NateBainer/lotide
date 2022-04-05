e2e9f090bebc0fd53dc0c594e78d142c35df63c2
Nathan Bain <nate.bain9@gmail.com>
2022-04-05 00:37:13 -0400

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("Assertion Passed 😜😜😜: [actual] === [expected]")
+      }; else (console.log('Assertion Failed 🥶🥶🥶: [actual] !== [expected]'))
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
