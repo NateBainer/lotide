b69dd9a5583b06105f3cc3837524d374e6148f74
Nathan Bain <nate.bain9@gmail.com>
2022-04-06 09:59:06 -0400

    push

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([3, 2, 1], [1, 2, 3]));
+console.log(eqArrays(['1', '2', '3'], ['1', '2', 3]));
+console.log(eqArrays(['1', '2', '3'], ['1', '2', '3']));
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed 😜😜😜: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed 🥶🥶🥶: ${array1} !== ${array2}`);
+  }
+};
+
+console.log(assertArraysEqual(eqArrays));
--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,7 @@
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
-    console.log("Assertion Passed 😜😜😜: [actual] === [expected]")
-      }; else (console.log('Assertion Failed 🥶🥶🥶: [actual] !== [expected]'))
+    console.log("Assertion Passed 😜😜😜: [actual] === [expected]");
+  } else (console.log('Assertion Failed 🥶🥶🥶: [actual] !== [expected]'));
 };
 
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
