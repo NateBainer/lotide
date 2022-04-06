1a20631a4edc06e3657204826240db4b0a48fdf7
Nathan Bain <nate.bain9@gmail.com>
2022-04-06 00:01:42 -0400

    first commit

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(array1, array2) {
+  if (array1 === array2) {
+    array2;
+    console.log("Assertion Passed 😜😜😜: [array1] === [array2]");
+  } else (console.log('Assertion Failed 🥶🥶🥶: [array1] !== [array2]'));
+};
+
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([3, 2, 1], [1, 2, 3]));
+console.log(eqArrays(['1', '2', '3'], ['1', '2', 3]));
+console.log(eqArrays(['1', '2', '3'], ['1', '2', '3']));
