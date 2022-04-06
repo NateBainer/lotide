14e03441d7c1c6c8c5afe6b0b633e584f475f1a2
Nathan Bain <nate.bain9@gmail.com>
2022-04-06 14:56:15 -0400

    linter fixes

--- without.js
+++ without.js
@@ -32,13 +32,13 @@ const without = function(array1, array2) {
 
   let arr = [];
 
-  if (eqArrays(array1, array2)) return arr; 
+  if (eqArrays(array1, array2)) return arr;
   
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        arr.push(array1[i]);
-      } 
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      arr.push(array1[i]);
     }
+  }
   return arr;
 };
 
