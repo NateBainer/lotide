575984272ce464f1ff0a85648e68fc6f26a3651c
Nathan Bain <nate.bain9@gmail.com>
2022-04-06 22:27:09 -0400

    first commit

---
+++ middle.js
@@ -0,0 +1,27 @@
+const assertEqual = function(array1, array2) {
+  if (array1 === array2) {
+    array2;
+    console.log("Assertion Passed 😜😜😜: [array1] === [array2]");
+  } else (console.log('Assertion Failed 🥶🥶🥶: [array1] !== [array2]'));
+};
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed 😜😜😜: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed 🥶🥶🥶: ${array1} !== ${array2}`);
+  }
+};
