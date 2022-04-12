60f4f85a1167060ec1b276d1200b0dd4658afec6
Nathan Bain <nate.bain9@gmail.com>
2022-04-12 16:36:08 -0400

    obj shorthand

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
+const fullBody = {
+  head,
+  tail,
+  middle
+};
+
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+ fullBody
 };
