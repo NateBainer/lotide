750fc7bfe836485178e7bff8f53d4084e9d87614
Nathan Bain <nate.bain9@gmail.com>
2022-04-11 23:15:57 -0400

    refac AssArrEq

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,7 @@
-const assertEqual = function(array1, array2) {
-  if (array1 === array2) {
-    array2;
-    console.log("Assertion Passed 😜😜😜: [array1] === [array2]");
-  } else (console.log('Assertion Failed 🥶🥶🥶: [array1] !== [array2]'));
-};
+const assertEqual = require("../assertEqual");
 
+const eqArrays = require("../eqArrays");
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length)
-    return false;
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i])
-      return false;
-  }
-  return true;
-};
 
 
 const assertArraysEqual = function(array1, array2) {
@@ -27,3 +13,5 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+module.exports = assertArraysEqual;
